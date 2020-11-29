8fa2f53d26984adcd8068f28fc775aef0fe0725a
Rob <rob@robs-imac.home>
2020-11-29 11:35:59 +0000

    Initial commit

---
+++ src/main/java/com/buggyrjh/pmtool/PmToolApplication.java
@@ -0,0 +1,13 @@
+package com.buggyrjh.pmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buggyrjh/pmtool/PmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buggyrjh.pmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
